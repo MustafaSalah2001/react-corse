7223a2c4fd86de72d48a6376e04b4c73bc6eb693
Mustafa_Salah <101256406+mustafasalah21@users.noreply.github.com>
2026-07-06 14:44:53 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,19 @@
-import { Fragment, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import Form from "./components/Form";
-import Prodects from "./components/Header/Prodectes/Prodects";
-import Header from "./components/Header/Header";
-import Button from "./components/Button";
+import { Fragment } from "react";
 import { useState } from "react";
-const App = () => {
-    
 
-  const prodects = [
-    {
-      title: "prodect1",
-      description: "this is prodect1",
-      price: 100,
-    },
-    {
-      title: "prodect2",
-      description: "this is prodect2",
-      price: 200,
-    },
-    {
-      title: "prodect3",
-      description: "this is prodect3",
-      price: 300,
-    },
-  ];
+const App = () => {
+  
+  //const [input, setInput] = useState('mosalah');
+
+  
 
   return (
     <>
-      <Button onClick={() => console.log("hi 1")}>
-        <span>Click me</span>
-      </Button>
-      <Button onClick={() => console.log("hi 2")}>
-        <span>Click me 2</span>
-      </Button>
-      <Header />
-      <Form />
-      <Prodects prodects={prodects} />
+      <h1>Hello</h1>
+      <input type="text" value={input} onChange={(e) => setInput(e.target.value)}
+      placeholder="Enter your name" />
+      <h1>my name is {input}</h1>
+    
     </>
   );
 };
